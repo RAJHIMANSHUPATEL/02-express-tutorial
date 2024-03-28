95ce63c1259aecef772a85ac1d1102149700d9b5
RAJHIMANSHUPATEL <blazehimanshuraj@gmail.com>
2024-03-28 22:45:13 +0530

    params, query and middleware

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('user hit the resourse')
-    res.send("Home Page")
+// req ==> middleware ==> res
+const logger = (req, res, next) => {
+    const method = req.method;
+    const url = req.url;
+    const time = new Date().getFullYear();
+    console.log(method, url, time)
+    // res.send("Testing")
+    // Either send your own response or pass it on to the next middleware
+    next()
+}
+
+app.get('/', logger, (req, res) => {
+    res.send("Home")
 })
-
-app.get('/about', (req, res) => [
-    res.send("About Page")
-])
-
-app.all('*', (req, res) => {
-    res.status(404).send("<h1>Resourse Not Found</h1>")
+app.get('/about', logger, (req, res) => {
+    res.send("About")
 })
 
 app.listen(5000, () => {
-    console.log('Server is listening on port 5000....')
+    console.log("Server is listening on localhost 5000...")
 })
-
-
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all -- handles all http verbs
-// app.use -- used for middleware
-// app.listen
